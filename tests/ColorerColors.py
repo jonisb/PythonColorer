47d3ac3734f660f499a628c5b631dd069257947a
jonisb <github.com@JsBComputing.se>
2021-03-23 01:16:15 +0100

    Update ColorerColors.py

--- tests/ColorerColors.py
+++ tests/ColorerColors.py
@@ -137,9 +137,11 @@ class MyHTMLParser2(HTMLParser):
 def initColors():
     parser = MyHTMLParser2()
     try:
-        parser.feed((pathlib.Path(os.environ['COLORER5CATALOG']).parent/'hrd'/'console'/'jonib.hrd').read_text())
+        #parser.feed((pathlib.Path(os.environ['COLORER5CATALOG']).parent/'hrd'/'console'/'jonib.hrd').read_text())
+        parser.feed((pathlib.Path(os.environ['COLORER5CATALOG']).parent/'hrd'/'console'/'blue.hrd').read_text())
     except KeyError:
-        parser.feed((pathlib.Path(pathlib.Path(r"C:\Users\jonib\.colorer5catalog").read_text()).parent/'hrd'/'console'/'jonib.hrd').read_text())
+        #parser.feed((pathlib.Path(pathlib.Path(r"C:\Users\jonib\.colorer5catalog").read_text()).parent/'hrd'/'console'/'jonib.hrd').read_text())
+        parser.feed((pathlib.Path(pathlib.Path(r"C:\Users\jonib\.colorer5catalog").read_text()).parent/'hrd'/'console'/'blue.hrd').read_text())
 
 
 def SetAnsiColor(region):
